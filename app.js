f96b6ef719f197f22a83ffd098560cf152a5208e
Alex Slaughter <alex@alexslaughter.com>
2019-10-23 12:48:16 -0700

    typo

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(webhookHandler);
 
 //Setup Github Webhook Handler
 webhookHandler.on('check_suite', function(repo, data) {
-    console.log(check_suite, data['action'], data['check_run']['status'], data['check_run']['conclusion']);
+    console.log('check_suite', data['action'], data['check_run']['status'], data['check_run']['conclusion']);
 });
 
 app.set('views', path.join(__dirname, 'views'));
