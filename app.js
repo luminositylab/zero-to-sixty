cda8e08b1a6126d90ff2468f44fa9fb4e0127fb8
Alex Slaughter <alex@alexslaughter.com>
2019-10-23 12:36:41 -0700

    pull more data

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(bodyParser.json());
 app.use(webhookHandler);
 
 //Setup Github Webhook Handler
-webhookHandler.on('*', function(event, repo, data) {
-    console.log(event, data['action']);
+webhookHandler.on('check_suite', function(event, repo, data) {
+    console.log(event, data['action'], data['check_run']['status'], data['check_run']['conclusion']);
 });
 
 app.set('views', path.join(__dirname, 'views'));
