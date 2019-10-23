cae7709be498f443d21171337f4cf2b77e02517a
Alex Slaughter <alex@alexslaughter.com>
2019-10-23 12:44:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(webhookHandler);
 
 //Setup Github Webhook Handler
 webhookHandler.on('check_suite', function(event, repo, data) {
-    console.log(event, data['action'], data['check_run']['status'], data['check_run']['conclusion']);
+    console.log(event, data);
+    //['action'], data['check_run']['status'], data['check_run']['conclusion']
 });
 
 app.set('views', path.join(__dirname, 'views'));
