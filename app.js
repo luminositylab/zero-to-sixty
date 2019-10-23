c5ad06228f704723d6bd6bb566229dc130db4e18
Alex Slaughter <alex@alexslaughter.com>
2019-10-23 12:02:36 -0700

    add webhook

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var GithubWebHook = require('express-github-webhook');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var webhookHandler = GithubWebHook({
+    path: '/webhook',
+    secret: 'secret'
+});
+
 var app = express();
+app.use(bodyParser.json());
+app.use(webhookHandler);
+
+//Setup Github Webhook Handler
+webhookHandler.on('*', function(event, repo, data) {
+    console.log(event, repo, data);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
